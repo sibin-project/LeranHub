43b97ee3069d60af5ad88fd6357716fa3de3e6be
sibin <sibinc36@email.com>
2025-11-15 16:46:53 +0530

    update

--- learnhub-backend/server.js
+++ learnhub-backend/server.js
@@ -14,9 +14,7 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -26,4 +24,11 @@ app.use("/api/notes", notesRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/enrollments", enrollmentRoutes);
 app.use('/api/contact', contactRoutes);
-app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT || 5000}`));
+app.get("/", (req, res) => {
+  res.status(200).send("Server active");
+});
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", timestamp: Date.now() });
+});
+
+app.listen(process.env.PORT || 10000, () => console.log(`Server running on port ${process.env.PORT || 5000}`));
